20d80500a2219b6070a589154ab4713c7162cef3
ezel <ezelbenitez07@gmail.com>
2026-05-23 18:53:22 -0300

    comparadores java

---
+++ fundamentos/operadores/Operadores.java
@@ -0,0 +1,21 @@
+public class Operadores {
+    public static void main(String[] args) {
+	// Operadores en java 
+	int a = 50;
+	int b = 10; 
+	int suma= a+b;
+	int resta= a-b;
+	int multi= a*b;
+	int div= a/b;
+	int modulo= a%b;
+	System.out.println("a : "+ a + "\nb : "+ b );
+	System.out.println("La suma es : " + suma + "\nLa resta es : " + resta + 
+		"\nLa multiplicacion es :" + multi + 
+		"\nLa division es : "+ div + 
+		"\nEl modulo es : "+ modulo);
+	System.out.println("a>b? : "+ (a>b) );
+	System.out.println("a<b? : "+ (a<b));
+	System.out.println("a=b? "+ (a==b));
+	System.out.println("a!=b? "+ (a!=b));
+    }
+}
--- fundamentos/operadores/README.md
+++ fundamentos/operadores/README.md
@@ -1,4 +1,4 @@
-# Operadores en java 
+# Operadores y comparadores en java 
 
 ## Suma, resta, multiplicacion, division y modulo 
 ``` bash 
@@ -8,4 +8,10 @@ int resultadod = a/b ;
 int resultadom = a*b;
 int resultadomo = a%b; 
 ```
+- El " % " te da el resto 
 
+## comparadores 
+``` bash
+a > b 
+- a mayor a b? 
+```
