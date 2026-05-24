33d7a366feeaaf6e4eef676da1df81ea89ac7b5e
ezel <ezelbenitez07@gmail.com>
2026-05-24 12:03:12 -0300

    arrays en java

---
+++ fundamentos/arrays/Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays {
+    public static void main(String[] args) {
+    	// Declaracion de array 
+	int [] numeros = { 1, 2, 3, 4 ,5 } ;
+	System.out.println("numero en la posicion 1 es : " +numeros[0]);
+
+    }
+}
---
+++ fundamentos/arrays/README.md
@@ -0,0 +1,10 @@
+# Arrays en java 
+
+## Declaracion de un array 
+``` bash 
+tipo_de_dato [] nombre_de_array = {elemento1, elemento2, elemento3, etc };
+```
+- Ejemplo 
+``` bash 
+int [] numeros = {1,2,3,4,5};
+``` 
