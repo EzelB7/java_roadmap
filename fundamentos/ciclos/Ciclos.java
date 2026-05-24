1637b7c3fc98879f7503fffbbd9c482c37f8ac75
ezel <ezelbenitez07@gmail.com>
2026-05-24 11:29:17 -0300

    ciclos en java

---
+++ fundamentos/ciclos/Ciclos.java
@@ -0,0 +1,11 @@
+public class Ciclos {
+    public static void main(String[] args) {
+	// ciclos for y while en java
+	// ciclo for estructura convencional
+	// for(inicio o desde ; fin o hasta; con paso,salto o incremento) 
+	for ( int i = 1; i <= 5; i++) {
+	    System.out.println(i);
+	
+	}
+    }
+}
---
+++ fundamentos/ciclos/README.md
@@ -0,0 +1,8 @@
+# Ciclos en java 
+
+## Ciclo for 
+- Estructura convencional 
+``` bash 
+for(inicio o desde ; fin o hasta ; con paso, salto o incremento) 
+``` 
+
