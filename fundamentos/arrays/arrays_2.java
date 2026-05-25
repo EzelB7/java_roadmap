fcf8270a38e380efb7ef9fb7de98abe295d90853
ezel <ezelbenitez07@gmail.com>
2026-05-25 20:03:40 -0300

    for each

---
+++ fundamentos/arrays/arrays_2.java
@@ -0,0 +1,12 @@
+public class arrays_2 {
+    public static void main(String[] args) {
+    	// Inicializacion de un array con tamano
+	int [ ] numeros = new int[5]; // inicializa un array con 0 , 0 , 0, 0 , 0 
+	
+	// recorrido for each 
+	for (int numero : numeros) { // para cada numero en " numeros "  
+	    System.out.print(numero + " ");
+	}
+	System.out.println("");
+    }
+}
