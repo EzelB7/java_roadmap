a124030004e3893d39f90643b579db21a809c685
ezel <ezelbenitez07@gmail.com>
2026-05-25 20:20:13 -0300

    funciones

---
+++ fundamentos/funciones/Funciones.java
@@ -0,0 +1,13 @@
+public class Funciones {
+    // las funciones van dentro de la clase y fuera del main.
+    public static void saludar (){
+	System.out.println("hola esto es una funcion invocada! ");
+    }
+
+    public static void main(String[] args) { 
+	// funcion main principal
+	saludar();
+
+
+}// del main
+}// de la clase
